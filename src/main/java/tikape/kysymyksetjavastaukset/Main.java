edf53e5bf1702d57999dfe99eedc08514846bba0
Jarkko Mämmelä <jarkko.poker@gmail.com>
2018-10-11 23:44:51 +0300

    adding port

--- src/main/java/tikape/kysymyksetjavastaukset/Main.java
+++ src/main/java/tikape/kysymyksetjavastaukset/Main.java
@@ -9,6 +9,9 @@ import spark.template.thymeleaf.ThymeleafTemplateEngine;
 public class Main {
 
     public static void main(String[] args) {
+        if (System.getenv("PORT") != null) {
+            Spark.port(Integer.valueOf(System.getenv("PORT")));
+        }
         System.out.println("yo");
         
         Spark.get("/", (req, res) -> {
